011cedbd11ac4522226a5991a93f03672a6da7df
2HannahMoore <hannah.moore@hannahmoore.local>
2016-06-28 18:59:01 -0400

    Made adjustments based on feedback
    It works now!! :)

--- js/index.js
+++ js/index.js
@@ -1,61 +1,31 @@
-$(document).ready (function(){
+$(document).ready(function(){
 
-  var city ='default';
-
-  $('.entry').submit(changeBackground);
+  $('#city-name').submit(changeBackground);
 
   function changeBackground(event){
     event.preventDefault();
-  }
 
-  $('#submit-btn').click(function(){
-    var city = $('.city-type').val();
-    $('#city-input').val('');
-    console.log(city);
+    var city = $('#city-type').val().toUpperCase();
 
-      if(city === "NYC" || city === "New York" || city === "New York City"){
-          $('body').attr('class', 'nyc');
-          } 
-          else if (city === "San Francisco" || city === "SF" || city === "Bay Area"){
-          $('body').attr('class', 'sf');
-          }
-          else if (city === "Los Angeles" || city === "LA" || city === "LAX"){
-            $('body').attr('class','la');
-          } 
-          else if (city === "Austin" || city ==="ATX"){
-            $('body').attr('class', 'austin');
-          }
-           else if (city === "Sydney" || city === "SYD") 
-            $('body').attr('class', 'sydney');
+    if(city === "NYC" || city === "NEW YORK" || city === "NEW YORK CITY") {
+        $('body').attr('class', 'nyc');
+        } 
+        else if (city === "SAN FRANCISCO" || city === "SF" || city === "BAY AREA"){
+        $('body').attr('class', 'sf');
+        }
+        else if (city === "LOS ANGELES" || city === "LA" || city === "LAX") {
+          $('body').attr('class','la');
+        } 
+        else if (city === "AUSTIN" || city ==="ATX") {
+          $('body').attr('class', 'austin');
+        }
+         else if (city === "SYDNEY" || city === "SYD") {
+          $('body').attr('class', 'sydney');
+        } else { 
+          $('body').attr('class', '');
+          $('.city-type').val('');
+        }
+
+      $('#city-type').val('');
+    }
 });
-});
-
-
-
-
-//$(function(){
-
- // var newyork = ["New York", "New York City", "NYC"]
-  //var sanfran = ["San Francisco", "SF", "Bay Area"]
- // var losangeles = ["Los Angeles", "LA", "LAX"]
-  //var sydney = ["Sydney","SYD"]
-
-  //function handleSubmit(event){
-   // event.preventDefault();
-   // console.log('form was submitted')
-
-//submitting new york to change background image
- // function newyork(){
-  //  $('newyork').submit-btn()
-  //  $('newyork').css("background-image", ".nyc")
-
-    //  }
-
-  //}
-//});
-
-//Notes:
-
-  //function submitCity()
-     // if ($("#city-type").val("New York" or "New York City" or "NYC") = "yes"){
-       // $()
